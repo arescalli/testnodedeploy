82044231830f14aa9d48d3a177d74663583dc7b7
Alessandro Rescalli <alessandro.rescalli@binarysystem.eu>
2016-02-13 14:04:45 +0100

    ignores

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer( (request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  response.end('working!!!!\n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
